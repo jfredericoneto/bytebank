4a1bd321e446132898ec1bc883cd1e17dd759b24
Neto <jfredericon@gmail.com>
2024-01-25 12:11:10 -0300

    Adicionado as funções de sacar e depositar na conta

--- src/main/java/dev/jfredericoneto/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/dev/jfredericoneto/bytebank/domain/conta/ContaDAO.java
@@ -106,4 +106,27 @@ public class ContaDAO {
         }
         return conta;
     }
+
+    public void alterar(Integer numero, BigDecimal valor) {
+        PreparedStatement ps;
+        String sql = "UPDATE conta SET saldo = ? WHERE numero = ?";
+
+        try {
+            ps = conn.prepareStatement(sql);
+
+            ps.setBigDecimal(1, valor);
+            ps.setInt(2, numero);
+
+            ps.execute();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }
+    }
 }
