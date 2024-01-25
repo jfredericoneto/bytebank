8fdd9044c67de92f9864dbb47324e0a487e78ec5
Neto <jfredericon@gmail.com>
2024-01-25 12:26:44 -0300

    Implementado função de encerra ruma conta deletando da base de dados

--- src/main/java/dev/jfredericoneto/bytebank/domain/conta/ContaService.java
+++ src/main/java/dev/jfredericoneto/bytebank/domain/conta/ContaService.java
@@ -70,7 +70,9 @@ public class ContaService {
             throw new RegraDeNegocioException("Conta não pode ser encerrada pois ainda possui saldo!");
         }
 
-        contas.remove(conta);
+        Connection conn = connection.recuperarConexao();
+
+        new ContaDAO(conn).deletar(numeroDaConta);
     }
 
     private Conta buscarContaPorNumero(Integer numero) {
